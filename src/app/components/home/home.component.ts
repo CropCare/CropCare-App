9b33ad2e19c96951c83da5be8efe2285f01e8319
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2024-02-07 03:03:20 +0200

    finish hero section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { NavbarSmComponent } from '../navbar-sm/navbar-sm.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarSmComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
