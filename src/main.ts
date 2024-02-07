58275f065d644b0d96f83e56719d4fd50ad5d107
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2024-02-08 00:59:13 +0200

    fix vercel speed insights

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { inject } from '@vercel/analytics';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { injectSpeedInsights } from "@vercel/speed-insights"
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
 
 inject();
-SpeedInsights({});
+injectSpeedInsights();
