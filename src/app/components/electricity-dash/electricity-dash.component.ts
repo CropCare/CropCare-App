56b9af6044103cc90462a4eb2aeaf0016c0d4640
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2024-02-20 01:41:22 +0200

    update dashboard

--- src/app/components/electricity-dash/electricity-dash.component.ts
+++ src/app/components/electricity-dash/electricity-dash.component.ts
@@ -5,8 +5,11 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './electricity-dash.component.html',
-  styleUrl: './electricity-dash.component.css'
+  styleUrl: './electricity-dash.component.css',
 })
 export class ElectricityDashComponent {
-
+  temperature = 32;
+  lightIntensity: number = 400;
+  conductivity: number = 300;
+  battery: number = 73;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,16 @@ module.exports = {
     extend: {
       colors: {
         primary: "#bff466",
-        "primary-light": "#f1fbed",
+        "primary-light": "#cbe1d5",
         secondary: "#124734",
-        tertiary: "#ebf9a4",
+        tertiary: "#edfc93",
         accent: "#ff8b11",
+        accent2: "#94d6d5",
         dark: "#1A2421",
         light: "#f2f8f6",
         gray: "#c0c4c3",
         "gray-dark": "#5a5a5a",
+        
       },
       fontFamily: {
         sans: ["Mona-Sans", "sans-serif"],
