e5b9f2c8d7ae8bc1df2c789353364fe5347d26ea
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2024-02-19 20:24:55 +0200

    dash nav now works

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { WaterDashComponent } from '../water-dash/water-dash.component';
+import { FertilizerDashComponent } from '../fertilizer-dash/fertilizer-dash.component';
+import { ElectricityDashComponent } from '../electricity-dash/electricity-dash.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    WaterDashComponent,
+    FertilizerDashComponent,
+    ElectricityDashComponent,
+    NgClass
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
+  currentTab = 'water';
 
+  constructor() {}
+
+  switchTab(tab: string) {
+    this.currentTab = tab;
+  }
 }
